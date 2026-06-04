349a0b59624c9863ccfbce9f10445ffed8bf7102
N1zuka <110715074+1798821508@users.noreply.github.com>
2026-06-04 19:12:40 +0800

    fix: Simplify settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,31 +1,11 @@
 pluginManagement {
     repositories {
-        maven("https://maven.fabricmc.net/")
         maven("https://maven.neoforged.net/releases/")
         mavenCentral()
         gradlePluginPortal()
     }
 }
 
-plugins {
-    id("dev.kikugie.stonecutter") version "0.7.10"
-}
-
 rootProject.name = "ClickMobs"
 
 include("fabric")
-
-stonecutter {
-    kotlinController = true
-    centralScript = "build.gradle.kts"
-    create("fabric") {
-        fun version(version: String, vararg loaders: String) {
-            loaders.forEach {
-                this.version("$version-$it", version)
-                    .buildscript = "build.$it.gradle.kts"
-            }
-        }
-        version("1.21.1", "neoforge")
-        vcsVersion = "1.21.1-neoforge"
-    }
-}
